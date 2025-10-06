9164e7c67b6539153e45f74ee319ed99e3c95ab3
WaficMikati <wrmikati@proton.me>
2025-10-06 01:35:25 -0500

    added navbar collapse button to make it responsive

--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   const { toggleTheme } = useTheme()
 
   return (
-    <nav className={`navbar navbar-expand bg-dark-subtle p-3`}>
+    <nav className={`navbar navbar-expand-md bg-dark-subtle p-3`}>
       <div className='container-fluid'>
         <Link
           className='nav-brand fs-4 text-decoration-none text-body'
@@ -15,48 +15,64 @@ export default function Navbar() {
         >
           Management Portal
         </Link>
-        <ul className='navbar-nav align-items-center ms-auto fs-6'>
-          <li className='nav-item'>
-            <Link
-              to='/'
-              className='nav-link'
-            >
-              Dashboard
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/users'
-              className='nav-link'
-            >
-              Users
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/orders'
-              className='nav-link'
-            >
-              Orders
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/products'
-              className='nav-link'
-            >
-              Products
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <button
-              className='nav-link'
-              onClick={toggleTheme}
-            >
-              <FontAwesomeIcon icon={faLightbulb} />
-            </button>
-          </li>
-        </ul>
+        <button
+          class='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span class='navbar-toggler-icon'></span>
+        </button>
+        <div
+          class='collapse navbar-collapse'
+          id='navbarNav'
+        >
+          <ul className='navbar-nav align-items-center ms-auto fs-6'>
+            <li className='nav-item'>
+              <Link
+                to='/'
+                className='nav-link'
+              >
+                Dashboard
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/users'
+                className='nav-link'
+              >
+                Users
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/orders'
+                className='nav-link'
+              >
+                Orders
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-link'
+              >
+                Products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <button
+                className='nav-link'
+                onClick={toggleTheme}
+              >
+                <FontAwesomeIcon icon={faLightbulb} />
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
